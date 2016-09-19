f2211b0df172126c16d3b2ed51b53c732f0a4487
Alec Cove <alec.cove@gmail.com>
2016-09-19 18:08:50 -0400

    Add L-System Explorer

---
+++ l-system-explorer/main.js
@@ -0,0 +1,21 @@
+
+/*
+ * Explorer. Main controller class for the app. Instantiated on window.onload
+ */
+var Explorer = function() {
+
+    var controls = new Controls();
+}
+
+
+
+/*
+ * Controls. Left sided controls drawer
+ */
+var Controls = function () {
+
+}
+
+
+
+window.onload = new Explorer();
