1499bbf96c3687b77dd20b390d7228306cd070b6
Alec Cove <alec.cove@gmail.com>
2016-09-20 23:06:38 -0400

    Add CSS animation to drawer toggling

---
+++ l-system-explorer/Application.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+/*
+ * Main application object for the L-Systems Explorer.
+ * Instantiated on window.onload
+ */
+var App = function () {
+    var controlPanel = new ControlPanel();
+}
+
+
+
+
+
+
+
+
+window.onload = new App();
